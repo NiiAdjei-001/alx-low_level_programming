0c04c63c88c10f45d2e3f59b987e4de6c90f4f54
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-03-17 02:53:14 +0000

    Saved. Slot 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i = 0;
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	while (i < sizeof(quote))
+	while (i < (int) sizeof(quote))
 	{
 		putchar(quote[i]);
 		i++;
