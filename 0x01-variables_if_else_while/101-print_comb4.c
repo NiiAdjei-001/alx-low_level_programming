913eb6ed80194314ff19484988ef1480c9bbde25
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-03-20 19:37:30 +0000

    Saved. Slot 1

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - Entry code
+ *
+ * Return: 0 (Succcess)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++){
+				if (i >= j || j >= k)
+				{
+					;
+				}
+				else
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if ((i + 1) < 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
