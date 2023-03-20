4310682b7b3c540c8287cea056379d28de8ffbcf
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-03-20 22:39:08 +0000

    Saved. Slot 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,39 +7,40 @@
  */
 int main(void)
 {
-	int i;/* first number ten's position*/
-	int j;/* first number one's position*/
-	int k;/* second number ten's position*/
-	int l;/*second number one's position*/
-	int condition1;/*condition case for: [ab ab]*/
-	int condition2;/*condition case for: ik > kl */
+	int n1, n11, n12;
+	int n2, n21, n22;
+	int cond1, cond2, cond3;
 
-	for (i = 48; i < 58; i++)
+	n1 = n11 = n12 = 0;
+	n2 = n21 = n22 = 0;
+	while (n1 < 100)
 	{
-		for (j = 48; j < 58; j++)
+		n11 = n1 / 10;
+		n12 = n1 % 10;
+		while (n2 < 100)
 		{
-			for (k = 48; k < 58; k++)
+			n21 = n2 / 10;
+			n22 = n2 % 10;
+			cond1 = (n11 == n21) && (n12 == n22);
+			cond2 = ((n11 > n21) || ((n11 == n21) && (n12 > n22)));
+			cond3 = !((n11 == 9) && (n12 == 8) && (n21 == 9) && (n22 == 9));
+			if (!(cond1 || cond2))
 			{
-				for (l = 48; l < 58; l++)
+				putchar(n11 + 48);
+				putchar(n12 + 48);
+				putchar(' ');
+				putchar(n21 + 48);
+				putchar(n22 + 48);
+				if (cond3)
 				{
-					condition1 = (i == k) && (j == l);
-					condition2 = ((i > k) || ((i == k) && (j > l)));
-					if (!(condition1 || condition2))
-					{
-						putchar(i);
-						putchar(j);
-						putchar(' ');
-						putchar(k);
-						putchar(l);
-						if (!((i == 57) && (j == 56) && (k == 57) && (l == 57)))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
+			n2++;
 		}
+		n1++;
+		n2 = 0;
 	}
 	putchar('\n');
 	return (0);
