bd86cce0ae73d2efc6206d8d6674becd31c4848b
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-03-20 21:47:54 +0000

    Saved. Slot 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,8 +12,7 @@ int main(void)
 	int k;/* second number ten's position*/
 	int l;/*second number one's position*/
 	int condition1;/*condition case for: [ab ab]*/
-	int condition2;/*condition case for: [ab ba]*/
-	int condition3;/*condition case for: ik > kl */
+	int condition2;/*condition case for: ik > kl */
 
 	for (i = 48; i < 58; i++)
 	{
@@ -24,9 +23,8 @@ int main(void)
 				for (l = 48; l < 58; l++)
 				{
 					condition1 = (i == k) && (j == l);
-					condition2 = (i == l) && (j == k);
-					condition3 = ((i > k) || ((i == k) && (j > l)));
-					if (!(condition1 || condition2 || condition3))
+					condition2 = ((i > k) || ((i == k) && (j > l)));
+					if (!(condition1 || condition2))
 					{
 						putchar(i);
 						putchar(j);
