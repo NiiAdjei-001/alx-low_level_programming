ab4a0a2bb6638a9ef887758d34d23420e25ca503
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-03-20 09:33:29 +0000

    Saved. Slot 1

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - Entry code
+ *
+ * Return: 0 (Succcess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
