929924bceb61c32c7527f6d3f328acd760d9364d
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-03-23 09:12:43 +0000

    Creating 0x02-functions_nested_loops direcotry

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry code
+ *
+ * Return: 0 (Succcess)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
