7b8d528ccab441399fd3f11891bf9c4dcc8f9ffd
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-03-26 19:34:48 +0000

    Fixed Task 14 Advanced

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,18 +6,18 @@
  */
 void fibonacci(void)
 {
-	int n1, n2, temp, i;
+	unsigned int n1, n2, temp, i;
 
 	i = 0;
 	n1 = 1;
 	n2 = 2;
-	printf("%d, %d, ", n1, n2);
+	printf("%u, %u, ", n1, n2);
 	while (i < 48)
 	{
 		temp = n1 + n2;
 		n1 = n2;
 		n2 = temp;
-		printf("%d, ", temp);
+		printf("%u, ", temp);
 		i++;
 	}
 	printf("\n");
