56eacb5c533afc21359a343bab29917718a689c5
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-03-28 14:45:01 +0000

    Completed Task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ * reset_to_98- Updates an integer address with a value 98.
+ *
+ * @n: An integer pointer.
+ *
+ * Description: Update integer address location with a value 98.
+ *
+ * Return: Nothing.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
