04498059d70aeb8449092ae3a941b3b36e982fa0
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-03-29 08:34:46 +0000

    Complted Tasks 1 & 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the content of integer pointers a and b.
+ * @a: Integer pointer a;
+ * @b: Integer pointer b;
+ * Return: Returns nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int valA;
+	int valB;
+	
+	valA = *a;
+	valB = *b;
+	
+	*a = valB;
+	*b = valA;
+}
