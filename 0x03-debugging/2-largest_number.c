7340823be5bf050dc351c032ec4adc164cb73d67
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-03-29 21:03:13 +0000

    Copleted Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	largest = a;
+
+	if (a > largest)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	if (b > largest)
 	{
 		largest = b;
 	}
-	else
+	if (c > largest)
 	{
 		largest = c;
 	}
