d5b36900254e09276e36a56cfa09feb12171c501
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-04-12 02:31:03 +0000

    Completed Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Create a grid.
+ * @width: width of grid.
+ * @height: height of grid.
+ * Return: returns a grid width x height.
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int c, r;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = (int **) malloc(height * sizeof(int *));
+	for (r = 0; r < height; r++)
+	{
+		grid[r] = (int *) malloc(width * sizeof(int));
+	}
+	if (grid == NULL)
+	{
+		fprintf(stderr, "Not enough memory available!\n");
+		return (NULL);
+	}
+	for (r = 0; r < height; r++)
+	{
+		for (c = 0; c < width; c++)
+		{
+			grid[r][c] = 0;
+		}
+	}
+	return (grid);
+}
