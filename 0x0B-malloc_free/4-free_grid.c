f74b27f631e9a8656f0c235b554cec21dcdbfa29
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-04-12 02:58:27 +0000

    Completed Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - release the memory of a grid/ 2D array.
+ * @grid: 2D array.
+ * @height: height of grid..
+ */
+
+void free_grid(int **grid, int height)
+{
+	int c, r;
+
+	if (grid != NULL)
+	{
+		for (r = 0; r < height; r++)
+		{
+			if (grid[r--] == NULL)
+			{
+				fprintf(stderr, "Not enough memory available!\n");
+				while (r >= 0)
+				{
+					free(grid[r--]);
+				}
+				free(grid);
+				return (NULL);
+			}
+		}
+	}
+}
