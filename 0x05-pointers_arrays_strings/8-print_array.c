494993475154d307827d879c952619a768c15da3
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-04-13 01:29:34 +0000

    Fixed Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,9 +7,17 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int i, len;
 
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", *(a + i));
-	printf("%d\n", *(a + (n - 1)));
+	len = 0;
+	while (*(a + len) != '\0';
+		len++;
+	if (n != len)
+		printf("\n");
+	else
+	{
+		for (i = 0; i < n - 1; i++)
+			printf("%d, ", *(a + i));
+		printf("%d\n", *(a + (n - 1)));
+	}
 }
