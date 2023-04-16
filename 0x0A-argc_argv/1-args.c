0c845f365958b67c72b40d94a0d2eeeb1aad9e9f
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-04-16 00:42:30 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: arguement count
+ * Return: Always return 0
+ */
+int main(int argc, char **argv)
+{
+	if (argv)
+	{
+		printf("%d\n", (argc - 1));
+	}
+	exit(EXIT_SUCCESS);
+}
