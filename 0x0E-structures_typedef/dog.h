d978ea51a898dd48f460747567e394642d424b51
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-04-18 02:10:51 +0000

    Initializing Project 0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
+/**
+ * struct dog - Dog object or data type
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ *
+ */
 struct dog
 {
 	char *name;
 	int age;
 	char *owner;
-}
+};
