14da50129832abd6e76b67457b7126211c6a79fd
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-04-18 03:13:57 +0000

    Completed Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,8 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		if (age)
-			printf("Age: %.6s\n", d->age);
+		if (d->age)
+			printf("Age: %.6f\n", d->age);
 		else
 			printf("Age: (nil)\n");
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
