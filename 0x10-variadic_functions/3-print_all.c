3846c40774b13d4aa592641a6b038c94075f489d
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-04-21 04:07:27 +0000

    Created Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_all - prints a list of arguments correponding to format type.
+ * @format: acceptable data type format. .
+ * @...: args
+ */
+void print_all(const char * const format, ...)
+{
+	printf("\n");
+}
