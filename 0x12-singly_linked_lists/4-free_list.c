b8b97b657c5eddcdd3d939aeba84c63a193dc39d
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-04-28 05:25:18 +0000

    Completed Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include"lists.h"
+/**
+ * free_from_tail - go to tail of the list and free memory
+ * @node : current node
+ */
+void free_from_tail(list_t *node)
+{
+	if (node->next != NULL)
+		free_from_tail(node->next);
+	else
+	{
+		free(node);
+		return;
+	}
+}
+/**
+ * free_list - clear list and release memory
+ * @head: head node pointer address
+ */
+void free_list(list_t *head)
+{
+
+	if (head->next == NULL)
+		free(head);
+	else
+		free_from_tail(head);
+}
