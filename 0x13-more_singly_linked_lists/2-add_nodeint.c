25614f6c8e493f68b4d77ebb6e766fc4df13e936
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-05-02 22:16:14 +0000

    Completed Task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,36 @@
+#include"lists.h"
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: address of head node.(listint **head)
+ * @n: integer value
+ * Returns: returns the address of the added node.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (*head == NULL)
+	{
+		new  = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		else
+		{
+			new->n = n;
+			*head = new;
+		}
+	}
+	else
+	{
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		else
+		{
+			new->n = n;
+			new->next = *head;
+			*head = new;
+		}
+	}
+	return (NULL);
+}
