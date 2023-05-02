b258c70ea3699cb8942167a0b35009014d3fe352
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-05-02 22:18:35 +0000

    Completed Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,6 +18,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		{
 			new->n = n;
 			*head = new;
+			return (new);
 		}
 	}
 	else
@@ -30,7 +31,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 			new->n = n;
 			new->next = *head;
 			*head = new;
+			return (new);
 		}
 	}
-	return (NULL);
 }
