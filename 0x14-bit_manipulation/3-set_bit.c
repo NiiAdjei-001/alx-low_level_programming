6b3424abae26c9cd19adeb1c8e09c349a0f4cbc4
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-05-06 05:17:20 +0000

    Task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * set_bit - sets a binary's bit number at a given index to 1
+ * @n: unsigned long integer pointer
+ * @index: index value
+ * Return: Returns the bit value at the given index.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bin;
+
+	if (n == NULL)
+		return (-1);
+	bin = 1;
+	bin <<= index;
+	*n |= bin;
+	return (*n);
+}
