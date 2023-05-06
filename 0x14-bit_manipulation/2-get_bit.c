cc2f88cd852b592da41d68472c01cfb38a13c85c
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-05-06 02:56:01 +0000

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,8 +8,18 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int i;
+	unsigned long int i, checker_n, checker_i;
 
+	checker_n = n;
+	checker_i = 0;
+	while (checker_n != 0)
+	{
+		if (checker_n / 2)
+			checker_i++;
+		checker_n /= 2;
+	}
+	if (index > checker_i)
+		return (-1);
 	for (i = 0; i < index ; i++)
 	{
 		n >>= 1;
