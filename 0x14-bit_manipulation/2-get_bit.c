919df869ff70f66c18064000a1b353a7389c09df
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-05-06 02:32:51 +0000

    Task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * get_bit - gets a binary's bit number at a given index
+ * @n: unsigned long integer
+ * index: index;
+ * Return: Returns the bit value at the given index.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i;
+
+	for (i = 0; i < index ; i++)
+	{
+		n >>= 1;
+	}
+	return (n & 1);
+}
