099d2131b8cbd74efeb37871b8d542cf04d4f9d5
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-05-23 12:39:44 +0000

    Fixed read check on Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,7 @@ void openfilesource(int *file_from_d, char **argv)
 	*file_from_d = open(argv[1], O_RDONLY);
 	if (*file_from_d == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		fprintf(stdout, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 }
@@ -39,7 +39,7 @@ void openfilesource(int *file_from_d, char **argv)
  */
 void openfilesink(int *file_to_d, char **argv)
 {
-	int flag = O_RDWR | O_CREAT | O_TRUNC;
+	int flag = O_WRONLY | O_CREAT | O_TRUNC;
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	*file_to_d = open(argv[2], flag, mode);
@@ -67,18 +67,24 @@ int main(int argc, char **argv)
 	checkargument(argc);
 	openfilesource(&file_from_d, argv);
 	openfilesink(&file_to_d, argv);
-	readcheck = read(file_from_d, buffer, buffer_size);
-	while (readcheck > 0)
+	while (1)
 	{
+		readcheck = read(file_from_d, buffer, buffer_size);
+		if (readcheck == -1)
+		{
+			fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		if (readcheck == 0)
+			break;
 		writecheck = dprintf(file_to_d, "%s", buffer);
 		if (writecheck == -1)
 		{
-			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
+			fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
 			exit(99);
 		}
 		free(buffer);
 		buffer = malloc(buffer_size * sizeof(char));
-		readcheck = read(file_from_d, buffer, buffer_size);
 	}
 	if (close(file_from_d) == -1)
 	{
