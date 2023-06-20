cc801569695478cfb43be7e6dbba9cc634955794
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-06-20 02:48:29 +0000

    Project 0x18 - task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - a + b
+ * @a: first number
+ * @b: second number
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - a - b
+ * @a: first number
+ * @b: second number
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+  return (a - b);
+}
+
+/**
+ * mul - a * b
+ * @a: first number
+ * @b: second number
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+  return (a + b);
+}
+
+/**
+ * div - a / b
+ * @a: first number
+ * @b: second number
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+  return (a / b);
+}
+
+/**
+ * mod - a % b
+ * @a: first number
+ * @b: second number
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+  return (a % b);
+}
