4488289f4196a6fc831819c09dfae8e799ec3691
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-09-09 03:38:18 +0000

    Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - gets the value from hash table @ht given key @key
+ * @ht: hash table
+ * @key: key
+ *
+ * Return: returns the value of @key or NULL if key doesnt exist
+ *
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
