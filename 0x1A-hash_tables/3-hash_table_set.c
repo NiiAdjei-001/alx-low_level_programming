91748f67752ad99ae0264ffd7aa51b414e9c4ca0
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-09-09 03:11:37 +0000

    Fixed task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (strcmp(key, "") == 0)
 		return (0);
-	index = key_index(key);
+	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
