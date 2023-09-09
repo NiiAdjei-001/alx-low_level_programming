3594abcd875627d9e28a85605fa4a634154fdc16
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-09-09 01:22:42 +0000

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
-	new = malloc(sizeof(hash_table_t) * size);
+	new = malloc((void*) size);
 	if (new)
 		return (new);
 	return (NULL);
