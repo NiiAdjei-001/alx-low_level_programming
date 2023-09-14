7159fd2678fafe85194c90cf182c6fd9836bac68
Nii Adjei - Vagrant Env <niiadjeiosae.technology@gmail.com>
2023-09-14 05:57:37 +0000

    Fixed Error in hash_table_get.c: key:NULL

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *current;
 
 	if (key == NULL)
-		return(NULL)
+		return(NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
